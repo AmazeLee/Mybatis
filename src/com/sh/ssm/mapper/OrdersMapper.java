bfc740937b9a5a4775608e2f0716b4c15d2ec318
AmazeLee <911711054@qq.com>
2017-11-10 21:52:24 +0800

    mybatis逆向工程

---
+++ src/com/sh/ssm/mapper/OrdersMapper.java
@@ -0,0 +1,30 @@
+package com.sh.ssm.mapper;
+
+import com.sh.ssm.pojo.Orders;
+import com.sh.ssm.pojo.OrdersExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OrdersMapper {
+    int countByExample(OrdersExample example);
+
+    int deleteByExample(OrdersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    List<Orders> selectByExample(OrdersExample example);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
